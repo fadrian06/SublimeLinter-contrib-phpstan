d34def8d19d31e2e3db0a7565fde1744339ab3d4
Trent Petersen <trentpetersen.523@gmail.com>
2018-04-30 11:26:27 -0500

    Try to support classes and functions better

--- linter.py
+++ linter.py
@@ -1,11 +1,13 @@
 from SublimeLinter.lint import Linter
 
-
-class phpstan(Linter):
-    cmd = 'phpstan analyse --errorFormat raw --no-progress --level max ${file}'
+class PhpStan(Linter):
+    cmd = 'phpstan', 'analyse', '--errorFormat=raw', '--no-progress', '${args}', '${file}'
     regex = r'^.*:(?P<line>[0-9]+):(?P<message>.+)'
     default_type = 'error'
     multiline = False
+    tempfile_suffix = '-'
     defaults = {
-        'selector': 'source.php'
+        'selector': 'source.php',
+        '--autoload-file': '${file}',
+        '--level': 'max',
     }
