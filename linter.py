5159a5350bff841ccfc7bffd2a3be616a755c2d7
grzegorz <grzegorz@e-gaming.cz>
2018-09-10 08:17:39 +0200

    Fix "Using the option --errorFormat is deprecated, use --error-format instead"

--- linter.py
+++ linter.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import Linter
 
 
 class PhpStan(Linter):
-    cmd = 'phpstan', 'analyse', '--errorFormat=raw', '--no-progress', '${args}', '${file}'
+    cmd = 'phpstan', 'analyse', '--error-format=raw', '--no-progress', '${args}', '${file}'
     regex = r'^.*:(?P<line>[0-9]+):(?P<message>.+)'
     default_type = 'error'
     multiline = False
