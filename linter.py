8e00e8f70f4c570eb7fa6bb0d2f7fde722140d20
tikki <tikki@users.noreply.github.com>
2018-12-26 18:37:47 +0100

    add option to use Composer's autoload file
    If "use_composer_autoload" is true (default) we search for an
    appropriate "autoload.php" file created by composer and load it
    with phpstan.

--- linter.py
+++ linter.py
@@ -1,21 +1,44 @@
+from os import path
+from shlex import quote
+
 from SublimeLinter.lint import Linter
 
 
 class PhpStan(Linter):
-    cmd = (
-        "phpstan",
-        "analyse",
-        "--error-format=raw",
-        "--no-progress",
-        "${args}",
-        "${file}",
-    )
     regex = r"^(?!Note: ).*:(?P<line>[0-9]+):(?P<message>.+)"
     default_type = "error"
     multiline = False
     tempfile_suffix = "-"
     defaults = {
         "selector": "source.php",
-        "--autoload-file": "${file}",
+        "use_composer_autoload": True,
         "--level": "max",
     }
+
+    def cmd(self):
+        settings = self.get_view_settings()
+
+        cmd = ["phpstan", "analyse"]
+        opts = ["--error-format=raw", "--no-progress"]
+
+        if settings.get("use_composer_autoload", True):
+            autoload_file = self.find_autoload_php(self.view.file_name())
+            if autoload_file:
+                opts.append("--autoload-file={}".format(quote(autoload_file)))
+
+        return cmd + opts + ["${args}", "--", "${file}"]
+
+    def find_autoload_php(self, file_path):
+        basedir = None
+        while file_path:
+            basedir = path.dirname(file_path)
+            composer_json = "{basedir}/composer.json".format(basedir=basedir)
+            composer_lock = "{basedir}/composer.lock".format(basedir=basedir)
+            autoload = "{basedir}/vendor/autoload.php".format(basedir=basedir)
+            if path.isfile(autoload) and (
+                path.isfile(composer_json) or path.isfile(composer_lock)
+            ):
+                return autoload
+            if basedir == file_path:
+                break
+            file_path = basedir
