5723fdca6a18b091fdecd678551376dcb62fd074
tikki <tikki@users.noreply.github.com>
2018-12-26 18:25:23 +0100

    use Black

--- linter.py
+++ linter.py
@@ -2,13 +2,20 @@ from SublimeLinter.lint import Linter
 
 
 class PhpStan(Linter):
-    cmd = 'phpstan', 'analyse', '--errorFormat=raw', '--no-progress', '${args}', '${file}'
-    regex = r'^(?!Note: ).*:(?P<line>[0-9]+):(?P<message>.+)'
-    default_type = 'error'
+    cmd = (
+        "phpstan",
+        "analyse",
+        "--errorFormat=raw",
+        "--no-progress",
+        "${args}",
+        "${file}",
+    )
+    regex = r"^(?!Note: ).*:(?P<line>[0-9]+):(?P<message>.+)"
+    default_type = "error"
     multiline = False
-    tempfile_suffix = '-'
+    tempfile_suffix = "-"
     defaults = {
-        'selector': 'source.php',
-        '--autoload-file': '${file}',
-        '--level': 'max',
+        "selector": "source.php",
+        "--autoload-file": "${file}",
+        "--level": "max",
     }
